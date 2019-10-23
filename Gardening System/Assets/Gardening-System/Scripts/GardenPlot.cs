77756d73196aeb3ed024952579da589c347d0f3a
Reakain <reakain@users.noreply.github.com>
2019-10-23 01:38:10 -0400

    Set up some initial logic scripting so in the morning I can start laying tiles out and building json

---
+++ Gardening System/Assets/Gardening-System/Scripts/GardenPlot.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CleverCrow.Fluid.BTs.Tasks;
+using CleverCrow.Fluid.BTs.Trees;
+using UnityEngine;
+
+public class GardenPlot : MonoBehaviour
+{
+    [SerializeField]
+    private BehaviorTree _tree;
+
+    bool isSeeded = false;
+    bool isTilled = false;
+    bool isWatered = false;
+
+    int days = 0;
+    int season = 0;
+
+    Plant plant;
+
+    void Awake()
+    {
+        _tree = new BehaviorTreeBuilder(gameObject)
+            .Selector()
+                .Condition("Is Tilled", () => isTilled)
+                .Sequence("Erode")
+                    .Condition("Is Seeded", () => !isSeeded )
+                    .Do("Erode", () =>
+                    {
+                        isTilled = false;
+                        // Change sprite
+                        return TaskStatus.Success;
+                    })
+                .End()
+                .Sequence("Grow")
+                    .Condition("Is Seeded", () => isSeeded)
+                    .Condition("In Season", () => season == plant.season)
+                    .Condition("Is Watered", () => isWatered)
+                    .Do("Grow", () =>
+                    {
+                        days++;
+                        if (days >= plant.currentStage.daysInStage)
+                        {
+                            days = 0;
+                            plant.GoToNextStage();
+                            // Update sprite
+                        }
+                        return TaskStatus.Success;
+                    })
+                .End()
+                .Sequence("Shrivel")
+                .End()
+            .End()
+            .Build();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _tree.Tick();
+    }
+
+    void NewDay()
+    {
+        // Get day count for grow
+        _tree.Tick();
+
+    }
+
+    void NewSeason()
+    {
+        // Get the new season
+        // change tile sprite
+    }
+
+    public void Water()
+    {
+        isWatered = true;
+        // Change tile sprite to water
+    }
+
+    public void Till()
+    {
+        isTilled = true;
+        // Change the tile sprite
+    }
+
+    public void Harvest()
+    {
+        if(plant.currentStage.harvestable)
+        {
+            // Get your produce
+            // Do something with the plot??
+        }
+    }
+}
---
+++ Gardening System/Assets/Gardening-System/Scripts/Plant.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant
+{
+    public int season { get; private set; }
+    public int numStages { get; private set; }
+
+    public PlantStage[] stages { get; private set; }
+
+    public PlantStage currentStage { get { return stages[stageIndex]; } }
+    private int stageIndex = 0;
+
+    public Plant()
+    {
+
+    }
+
+    public void GoToNextStage()
+    {
+        // Increment our current stage
+        if (stageIndex < stages.Length - 1)
+        {
+            stageIndex++;
+        }
+    }
+}
+
+public class PlantStage
+{
+    public int stageNum { get; private set; }
+    public int daysInStage { get; private set; }
+    public bool harvestable { get; private set; }
+
+    public PlantStage(int stage, int days, bool canHarvest)
+    {
+        stageNum = stage;
+        daysInStage = days;
+        harvestable = canHarvest;
+    }
+}
